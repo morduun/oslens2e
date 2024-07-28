5337f1f12e0a1458a80c30fb7f8d810b27cb3da6
morduun <morduun@gmail.com>
2024-07-28 15:09:03 -0400

    Add files via upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const OS_LENS_2E = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+OS_LENS_2E.abilities = {
+  str: 'OS_LENS_2E.Ability.Str.long',
+  dex: 'OS_LENS_2E.Ability.Dex.long',
+  con: 'OS_LENS_2E.Ability.Con.long',
+  int: 'OS_LENS_2E.Ability.Int.long',
+  wis: 'OS_LENS_2E.Ability.Wis.long',
+  cha: 'OS_LENS_2E.Ability.Cha.long',
+};
+
+OS_LENS_2E.abilityAbbreviations = {
+  str: 'OS_LENS_2E.Ability.Str.abbr',
+  dex: 'OS_LENS_2E.Ability.Dex.abbr',
+  con: 'OS_LENS_2E.Ability.Con.abbr',
+  int: 'OS_LENS_2E.Ability.Int.abbr',
+  wis: 'OS_LENS_2E.Ability.Wis.abbr',
+  cha: 'OS_LENS_2E.Ability.Cha.abbr',
+};
